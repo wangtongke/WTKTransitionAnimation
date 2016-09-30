a8973fe22ece2bd4b9de49c40d033ceac287ae3b
wangtongke <81520140@qq.com>
2016-09-30 11:54:29 +0800

    添加md

---
+++ WTKTransitionAnimation/WTKTransitionAnimation/WTKTouchManager.h
@@ -0,0 +1,13 @@
+//
+//  WTKTouchManager.h
+//  WTKTransitionAnimation
+//
+//  Created by 王同科 on 16/9/29.
+//  Copyright © 2016年 王同科. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WTKTouchManager : NSObject
+
+@end
---
+++ WTKTransitionAnimation/WTKTransitionAnimation/WTKTouchManager.m
@@ -0,0 +1,13 @@
+//
+//  WTKTouchManager.m
+//  WTKTransitionAnimation
+//
+//  Created by 王同科 on 16/9/29.
+//  Copyright © 2016年 王同科. All rights reserved.
+//
+
+#import "WTKTouchManager.h"
+
+@implementation WTKTouchManager
+
+@end
